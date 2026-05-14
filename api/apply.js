1f197c4d77fd7906e36ff4e25fd265ca58299942
olatunjimoses12-prog <olatunjimoses12@gmail.com>
2026-05-14 08:22:58 -0700

    Fix email transporter setup (Zoho SMTP)

--- api/apply.js
+++ api/apply.js
@@ -11,14 +11,14 @@ export default async function handler(req, res) {
 
   // ================= EMAIL TRANSPORTER =================
   const transporter = nodemailer.createTransport({
-  host: "smtp.zoho.com",
-  port: 587,
-  secure: false, // use TLS
-  auth: {
-    user: "info@worldclasstechhub.com",
-    pass: "41414141aA@"
-  }
-});
+    host: "smtp.zoho.com",
+    port: 465,
+    secure: true,
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS
+    }
+  });
 
   try {
 
